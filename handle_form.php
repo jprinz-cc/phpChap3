68874aee10bd3bba620ad97f055a05b7b1072301
JP <jp@jp.com>
2017-06-14 21:37:05 -0400

    Broke code and removed error messaging

--- handle_form.php
+++ handle_form.php
@@ -21,9 +21,9 @@
 
 <body>
     <?php
-    ini_set('display_errors', 1);
+    /*ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
+    error_reporting(E_ALL);*/
 
 
     $title = $_POST['title'];
@@ -32,7 +32,7 @@
     $comments = $_POST['comments'];
 
     print "<p>Thank you, $title $name, for your comments.</p>
-    <p>You stated that you found this example to be '$response' and added: <br>$comments</p>"
+    <p>You stated that you found this example to be '$response' and added: <br><br>$comments</p>"
 
 
     ?>
