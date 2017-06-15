b20d3c1b74e07733ef6871c7d7d0894846c62f14
JP <jp@jp.com>
2017-06-14 22:12:34 -0400

    Fixed code and commented display_error;

--- handle_form.php
+++ handle_form.php
@@ -21,9 +21,9 @@
 
 <body>
     <?php
-    ini_set('display_errors', 1);
+    /*ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
+    error_reporting(E_ALL);*/
 
 
     $title = $_POST['title'];
@@ -31,7 +31,7 @@
     $response = $_POST['response'];
     $comments = $_POST['comments'];
 
-    print "<p>Thank you, $title $names, for your comments.</p>
+    print "<p>Thank you, $title $name, for your comments.</p>
     <p>You stated that you found this example to be '$response' and added: <br><br>$comments</p>"
 
 
