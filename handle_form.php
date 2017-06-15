366c704b139bcd9fa7c3494c9a85ddd8c04831a0
JP <jp@jp.com>
2017-06-14 21:49:19 -0400

    Added back comments

--- handle_form.php
+++ handle_form.php
@@ -21,9 +21,9 @@
 
 <body>
     <?php
-    /*ini_set('display_errors', 1);
+    ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);*/
+    error_reporting(E_ALL);
 
 
     $title = $_POST['title'];
