e9d3ea7996b5976467e35bf61a262aeb2671f227
JP <jp@jp.com>
2017-06-15 09:48:20 -0400

    Broke code, enabled errors

--- handle_form.php
+++ handle_form.php
@@ -21,13 +21,18 @@
 
 <body>
 
+
+
     <?php
+
+    ini_set('display_errors', 1);
+
     $title = $_POST['title'];
     $name = $_POST['name'];
     $response = $_POST['response'];
     $comments = $_POST['comments'];
 
-    print "<p>Thank you, $title $name, for your comments.</p>
+    print "<p>Thank you, $title $names, for your comments.</p>
     <p>You stated that you found this example to be '$response' and added:<br><br>$comments</p>";
 
 
